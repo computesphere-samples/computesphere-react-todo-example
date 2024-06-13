dadab80cc9254d618da89a55e1b233b033b49096
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2024-06-13 23:51:16 +0500

    style: update Tailwind config and globals css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,19 +4,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#F5F9FCD6",
-        secondary: "#E5E7EB",
-        "content-text": "#434C52",
-        "color-links": "#355DEE",
-        "color-white": "#fff",
-        gradient:
-          "rgba(178, 248, 240, 0.78) -9.55%, rgba(148, 220, 251, 0.8958) 4.42%, rgba(90, 152, 246, 0.97235) 33.3%, #073099 97.82%",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        "dark-blue": "var(--dark-blue)",
+        "dark-gray": "var(--dark-gray)",
+        "link-color": "var(--link-color)",
+        gradient: "var(--gradient-color)",
       },
       fontFamily: {
         sans: ["Poppins", "Arial", "Helvetica", "sans-serif"],
       },
       backgroundImage: {
         "main-gradient": "linear-gradient(180deg, #F8FAFC 0%, #ECFEFF 100%)",
+        "logo-gradient": "linear-gradient(0deg, var(--gradient-color))",
+        watermark: "url(assets/background-img.png)",
       },
     },
   },
