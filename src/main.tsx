655221cbec41e12aac9fe4bf75b1553970f7812b
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2024-06-13 23:28:32 +0500

    feat: create context for managing todos state

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./globals.css";
+import { TodoProvider } from "./context/TodoContext.tsx";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <TodoProvider>
+      <App />
+      <Toaster position='bottom-right' />
+    </TodoProvider>
   </React.StrictMode>
 );
